d1a53c112352d6772e17783f599a3b19a3d7ef7f
LeañoMarkVincent <mrkvncnt147369@gmail.com>
2022-04-11 13:20:56 +0800

    Song 1 Final

---
+++ Song 1.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Melodicalyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="Song 4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="Song 5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melodicalyrics</h1>
+                <p class="lead"> Lyrics in your own imagination.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                     
+                        <?php
+
+                        echo "<h2 class='text-primary'>Come Inside of My Heart</h2>";
+                        echo "<p class='lead '>IV of Spades</p>";
+                
+                        echo "<p> I love you </p>";
+                        echo "<p> But I don't really show you</p>";
+                        echo "<p> I'd call you</p>";
+                        echo "<p> But only if you want me too</p>";
+                        echo "<br/>";
+
+                        echo "<p> Oh, don't you let it stop</p>";
+                        echo "<p> Oh, I won't let it happen, baby</p>";
+                        echo "<p> I will never stop</p>";
+                        echo "<p> But only if you listen to me</p>";
+                        echo "<br/>";
+
+                        echo "<p> Come inside of my heart</p>";
+                        echo "<p> If you're looking for answers</p>";
+                        echo "<p> Look at the stars, go a little bit faster</p>";
+                        echo "<p> Ooh, ooh, hooh-ahh-ooh</p>";
+                        echo "<br/>";
+
+                        echo "<p> Come inside of my heart</p>";
+                        echo "<p> If you're looking for answers</p>";
+                        echo "<p> Look at the stars, go a little bit faster</p>";
+                        echo "<p> Ooh, ooh, hooh-ahh-ooh</p>";
+                        echo "<br/>";
+
+                        echo "<p> Oh, baby</p>";
+                        echo "<p> Forgive me if I hurt you</p>";
+                        echo "<p> Come save me</p>";
+                        echo "<p> 'Cause you're the only one for me</p>";
+                        echo "<br/>";
+
+                        echo "<p> Oh, don't you let it stop</p>";
+                        echo "<p> Oh, I won't let it happen, baby</p>";
+                        echo "<p> I will never stop</p>";
+                        echo "<p> But only if you listen to me</p>";
+                        echo "<br/>";
+
+                        echo "<p> Come inside of my heart</p>";
+                        echo "<p> If you're looking for answers</p>";
+                        echo "<p> Look at the stars, go a little bit faster</p>";
+                        echo "<p> Ooh, ooh, hooh-ahh-ooh</p>";
+                        echo "<br/>";
+
+                        echo "<p> Come inside of my heart</p>";
+                        echo "<p> If you're looking for answers</p>";
+                        echo "<p> Look at the stars, go a little bit faster</p>";
+                        echo "<p> Ooh, ooh, hooh-ahh-ooh</p>";
+                        echo "<br/>";
+
+                        echo "<p> Whatever happens to me, baby</p>";
+                        echo "<p> I'm sorry</p>";
+                        echo "<p> No one could ever go my way</p>";
+                        echo "<p> I love you</p>";
+                        echo "<p> But I don't really show you</p>";
+                        echo "<br/>";
+
+                        echo "<p> Come inside of my heart</p>";
+                        echo "<p> If you're looking for answers</p>";
+                        echo "<p> Look at the stars, go a little bit faster</p>";
+                        echo "<p> Ooh, ooh, hooh-ahh-ooh</p>";
+                        echo "<br/>";
+
+                        echo "<p> Come inside of my heart</p>";
+                        echo "<p> If you're looking for answers</p>";
+                        echo "<p> Look at the stars, go a little bit faster</p>";
+                        echo "<p> Ooh, ooh, hooh-ahh-ooh</p>";
+                        echo "<br/>";
+
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Mark Vincent R. Leaño</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+    Back to main page <a href = "index.php">HERE!</a>
+    </body>
+</html>
