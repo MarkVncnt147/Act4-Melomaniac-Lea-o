899e3967edef11fcfe3cdf5bdee42c960b15866f
LeañoMarkVincent <mrkvncnt147369@gmail.com>
2022-04-11 13:33:40 +0800

    Song 5 Final

---
+++ Song 5.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Melodicalyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="Song 4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="Song 5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melodicalyrics</h1>
+                <p class="lead"> Lyrics in your own imagination.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'> Habang Buhay </h2>";
+                        echo "<p class='lead '> Zack Tabuldo </p>";
+                
+                        echo "<p> Aking sinta</p>";
+                        echo "<p> Ano bang mayro'n sa iyo</p>";
+                        echo "<p> 'Pag nakikita ka na</p>";
+                        echo "<p> Bumabagal ang mundo</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Pag ngumingiti ka para bang may iba</p>";
+                        echo "<p> 'Pag tumitingin sa'kin mapupungay mong mga mata</p>";
+                        echo "<p> Wala akong takas sa nakakalunod mong ganda (ha)</p>";
+                        echo "<p> Halika nga (ha ha ha)</p>";
+                        echo "<br/>";
+
+                        echo "<p> Tingnan mo lang ang aking mga mata</p>";
+                        echo "<p> 'Wag kang titingin na sa iba</p>";
+                        echo "<p> Akin ka na wala nang iba (ha ha ha)</p>";
+                        echo "<p> Andito 'ko hanggang sa 'ting pagtanda</p>";
+                        echo "<br/>";
+
+                        echo "<p> Mamahalin kita basta't 'pag nahulog</p>";
+                        echo "<p> Nakahawak ako 'wag ka lang bibitaw</p>";
+                        echo "<p> Habang buhay na ako'y iyo</p>";
+                        echo "<p> Wala nang ibang nakagawa sa'kin nang ganito</p>";
+                        echo "<p> Kung 'di ikaw nag-iisang diyosa ng buhay ko</p>";
+                        echo "<br/>";
+
+                        echo "<p> 'Wag ka nang matakot 'wag kang mangamba</p>";
+                        echo "<p> Andito ako 'pag ika'y mag-isa</p>";
+                        echo "<p> Wala akong takas sa nakakalunod mong ganda</p>";
+                        echo "<p> Halika nga (ha ha ha)</p>";
+                        echo "<br/>";
+
+                        echo "<p> Tingnan mo lang ang aking mga mata</p>";
+                        echo "<p> 'Wag kang titingin na sa iba</p>";
+                        echo "<p> Akin ka na wala nang iba</p>";
+                        echo "<br/>";
+
+                        echo "<p> Andito 'ko hanggang sa 'ting pagtanda</p>";
+                        echo "<p> Mamahalin kita basta't 'pag nahulog</p>";
+                        echo "<p> Nakahawak ako 'wag ka lang bibitaw</p>";
+                        echo "<p> Habang buhay na ako'y iyo</p>";
+                        echo "<br/>";
+
+                        echo "<p> At kung 'di kumbinsido'y magtiwala ka</p>";
+                        echo "<p> Hawakan ang puso't maniwala</p>";
+                        echo "<p> Na ikaw lang ang s'yang inibig</p>";
+                        echo "<p> Ikaw lang ang iibigin,sinta</p>";
+                        echo "<br/>";
+
+                        echo "<p> Kahit ang likod mo'y kubang-kuba na</p>";
+                        echo "<p> Kahit ang ulo mo'y puro uban na</p>";
+                        echo "<p> Isasayaw ka hanggang sa pikit na ang ating mga mata (ha ha ha)</p>";
+                        echo "<p> Wala naman akong hiling pa</p>";
+                        echo "<p> Basta't kasama ka habang buhay na</p>";
+                        echo "<p> Kuntento ako basta't ikaw lang kasama ikaw kasama ko</p>";
+                        echo "<br/>";
+
+                        echo "<p> Aking sinta ano bang mayro'n sa iyo</p>";
+                        echo "<p> 'Pag nakikita ka na bumabagal ang mundo</p>";
+                        echo "<br/>";
+
+                        echo "<p> Tingnan mo lang ang aking mga mata</p>";
+                        echo "<p> 'Wag kang titingin na sa iba</p>";
+                        echo "<p> Akin ka na wala nang iba</p>";
+                        echo "<br/>";
+
+                        echo "<p> Andito 'ko hanggang sa 'ting pagtanda</p>";
+                        echo "<p> Mamahalin kita basta't 'pag nahulog</p>";
+                        echo "<p> Nakahawak ako 'wag ka lang bibitaw</p>";
+                        echo "<p> Habang buhay na ako'y iyo</p>";
+                        echo "<br/>";
+
+                        echo "<p> Tingnan mo lang ang aking mga mata</p>";
+                        echo "<p> 'Wag kang titingin na sa iba</p>";
+                        echo "<p> Akin ka na wala nang iba</p>";
+                        echo "<br/>";
+
+                        ?>
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Mark Vincent R. Leaño</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+    Back to main page <a href = "index.php">HERE!</a>
+    </body>
+</html>
