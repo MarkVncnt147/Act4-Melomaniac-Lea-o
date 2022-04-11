efb13bd579630c011a8188c62933e47b4dba9448
LeañoMarkVincent <mrkvncnt147369@gmail.com>
2022-04-11 13:28:53 +0800

    Song 3 Final

---
+++ Song 3.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Melodicalyrics</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="Song 3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="Song 4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="Song 5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melodicalyrics</h1>
+                <p class="lead"> Lyrics in your own imagination.</p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'> Stereo Hearts </h2>";
+                        echo "<p class='lead '> Gym Class Heroes ft. Adam Levine </p>";
+                
+                        echo "<p> My hearts a stereo</p>";
+                        echo "<p> It beats for you, so listen close</p>";
+                        echo "<p> Hear my thoughts in every note, oh oh</p>";
+                        echo "<p> Make me your radio</p>";
+                        echo "<p> Turn me up when you feel low</p>";
+                        echo "<p> This melody was meant for you</p>";
+                        echo "<p> Just sing along to my stereo</p>";
+                        echo "<br/>";
+
+                        echo "<p> Gym Class Hero, baby!</p>";
+                        echo "<p> If I was just another dusty record on the shelf</p>";
+                        echo "<p> Would you blow me off and play me like everybody else?</p>";
+                        echo "<p> If I asked you to scratch my back, could you manage that?</p>";
+                        echo "<p> Like it read well, check it Travie, I can handle that</p>";
+                        echo "<br/>";
+
+                        echo "<p> Furthermore, I apologize for any skipping tracks</p>";
+                        echo "<p> It's just the last girl that played me left a couple cracks</p>";
+                        echo "<p> I used to, used to, used to, used to, now I'm over that</p>";
+                        echo "<p> 'Cause holding grudges over love is ancient artifacts</p>";
+                        echo "<br/>";
+
+                        echo "<p> If I could only find a note to make you understand</p>";
+                        echo "<p> I'd sing it softly in your ear and grab you by the hand</p>";
+                        echo "<p> Just keep it stuck inside your head, like your favorite tune</p>";
+                        echo "<p> And know my hearts a stereo that only plays for you</p>";
+                        echo "<br/>";
+
+                        echo "<p> My hearts a stereo</p>";
+                        echo "<p> It beats for you, so listen close</p>";
+                        echo "<p> Hear my thoughts in every note, oh oh</p>";
+                        echo "<p> Make me your radio</p>";
+                        echo "<p> Turn me up when you feel low</p>";
+                        echo "<p> This melody was meant for you</p>";
+                        echo "<p> Just sing along to my stereo</p>";
+                        echo "<p> Oh oh oh oh oh oh to my stereo</p>";
+                        echo "<p> Oh oh oh to sing along to my stereo</p>";
+                        echo "<br/>";
+
+                        echo "<p> Let's go!</p>";
+                        echo "<p> If I was an old-school fifty pound boombox</p>";
+                        echo "<p> Would you hold me on your shoulder, wherever you walk?</p>";
+                        echo "<p> Would you turn my volume up in front of the cops?</p>";
+                        echo "<p> And crank it higher everytime they told you to stop</p>";
+                        echo "<br/>";
+
+                        echo "<p> And all I ask is that you dont get mad at me</p>";
+                        echo "<p> When you have to purchase mad D batteries</p>";
+                        echo "<p> Appreciate every mixtape your friends make</p>";
+                        echo "<p> You never know we come and go like on the interstate</p>";
+                        echo "<br/>";
+
+                        echo "<p> If I could only find a note to make you understand</p>";
+                        echo "<p> I'd sing it softly in your ear and grab you by the hand</p>";
+                        echo "<p> Just keep it stuck inside your head, like your favorite tune</p>";
+                        echo "<p> And know my hearts a stereo that only plays for you</p>";
+                        echo "<br/>";
+
+                        echo "<p> My hearts a stereo</p>";
+                        echo "<p> It beats for you, so listen close</p>";
+                        echo "<p> Hear my thoughts in every note, oh oh</p>";
+                        echo "<p> Make me your radio</p>";
+                        echo "<p> Turn me up when you feel low</p>";
+                        echo "<p> This melody was meant for you</p>";
+                        echo "<p> Just sing along to my stereo</p>";
+                        echo "<p> Oh oh oh oh oh oh to my stereo</p>";
+                        echo "<p> Oh oh oh to sing along to my stereo</p>";
+                        echo "<br/>";
+
+                        echo "<p> I only pray you'll never leave me behind (never leave me)</p>";
+                        echo "<p> Because good music can be so hard to find (so hard to find)</p>";
+                        echo "<p> I take your hand and pull it closer to mine (yeah)</p>";
+                        echo "<p> Thought love was dead, but now you're changing my mind</p>";
+                        echo "<br/>";
+
+                        echo "<p> My hearts a stereo</p>";
+                        echo "<p> It beats for you, so listen close</p>";
+                        echo "<p> Hear my thoughts in every note, oh oh</p>";
+                        echo "<p> Make me your radio</p>";
+                        echo "<p> Turn me up when you feel low</p>";
+                        echo "<p> This melody was meant for you</p>";
+                        echo "<p> Just sing along to my stereo</p>";
+                        echo "<p> Oh oh oh oh oh oh to my stereo</p>";
+                        echo "<p> Oh oh oh to sing along to my stereo</p>";
+                        echo "<br/>";
+
+                        ?>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Mark Vincent R. Leaño</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+    Back to main page <a href = "index.php">HERE!</a>
+    </body>
+</html>
